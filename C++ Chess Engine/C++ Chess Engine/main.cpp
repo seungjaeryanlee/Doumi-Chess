f2fa509444354b8598f0fde41b8ee20f6358b437
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2015-10-25 03:47:48 -0400

    Board Representation Completed
    Using 120 size integer array (might change to short). Enum created and
    basic structure for code also added.

--- C++ Chess Engine/C++ Chess Engine/main.cpp
+++ C++ Chess Engine/C++ Chess Engine/main.cpp
@@ -1,5 +1,128 @@
+/*                                    DEFINITION                              */
 #include <stdio.h>
 
-void main() {
+/*                              FUNCTION DECLARATION                          */
+//  This function sets up currentboard[120] for the initial position of pieces.
+void board120Setup();
+//  This functions prints the board from the parameter.
+void printBoard(int board[120]);
+
+/*                                   ENUMERATION                              */
+enum squareType {
+     EMPTYSQUARE,
+     WHITEPAWN, WHITEKNIGHT, WHITEBISHOP, WHITEROOK, WHITEQUEEN, WHITEKING,
+     BLACKPAWN, BLACKKNIGHT, BLACKBISHOP, BLACKROOK, BLACKQUEEN, BLACKKING,
+     ERRORSQUARE
+};
+enum fileRank120 {
+     A8=21, B8, C8, D8, E8, F8, G8, H8,
+        A7, B7, C7, D7, E7, F7, G7, H7,
+        A6, B6, C6, D6, E6, F6, G6, H6,
+        A5, B5, C5, D5, E5, F5, G5, H5,
+        A4, B4, C4, D4, E4, F4, G4, H4,
+        A3, B3, C3, D3, E3, F3, G3, H3,
+        A2, B2, C2, D2, E2, F2, G2, H2,
+        A1, B1, C1, D1, E1, F1, G1, H1
+};
+
+/*                                 GLOBAL VARIABLE                            */
+int currentBoard[120];
+
+
+/*                                    FUNCTION                                */
+void board120Setup() {
+     //  Add Empty Squares
+     for (int i = 0; i < 8; i++) {
+          for (int j = 0; j < 8; j++) {
+               currentBoard[(i + 2) * 10 + j + 1] = EMPTYSQUARE;
+          }
+     }
+     
+     //  Add Error Squares
+     for (int j = 0; j < 10; j++) {
+          currentBoard[j] = ERRORSQUARE;
+          currentBoard[10 + j] = ERRORSQUARE;
+          currentBoard[100 + j] = ERRORSQUARE;
+          currentBoard[110 + j] = ERRORSQUARE;
+     }
+     for (int j = 0; j < 12; j++) {
+          currentBoard[10 * j] = ERRORSQUARE;
+          currentBoard[10 * j + 9] = ERRORSQUARE;
+     }
+     
+     //  Add Chess Pieces
+     currentBoard[A8] = BLACKROOK;
+     currentBoard[B8] = BLACKKNIGHT;
+     currentBoard[C8] = BLACKBISHOP;
+     currentBoard[D8] = BLACKQUEEN;
+     currentBoard[E8] = BLACKKING;
+     currentBoard[F8] = BLACKBISHOP;
+     currentBoard[G8] = BLACKKNIGHT;
+     currentBoard[H8] = BLACKROOK;
+
+     currentBoard[A1] = WHITEROOK;
+     currentBoard[B1] = WHITEKNIGHT;
+     currentBoard[C1] = WHITEBISHOP;
+     currentBoard[D1] = WHITEQUEEN;
+     currentBoard[E1] = WHITEKING;
+     currentBoard[F1] = WHITEBISHOP;
+     currentBoard[G1] = WHITEKNIGHT;
+     currentBoard[H1] = WHITEROOK;
+}
+void printBoard(int board[120]) {
+     for (int i = 0; i < 120; i++) {
+          if (i % 10 == 0) {
+               printf("\n");
+          }
+          switch (board[i]) {
+          case ERRORSQUARE:
+               printf("X ");
+               break;
+          case EMPTYSQUARE:
+               printf("- ");
+               break;
+          case WHITEPAWN:
+               printf("P ");
+               break;
+          case WHITEKNIGHT:
+               printf("N ");
+               break;
+          case WHITEBISHOP:
+               printf("B ");
+               break;
+          case WHITEROOK:
+               printf("R ");
+               break;
+          case WHITEQUEEN:
+               printf("Q ");
+               break;
+          case WHITEKING:
+               printf("K ");
+               break;
+          case BLACKPAWN:
+               printf("p ");
+               break;
+          case BLACKKNIGHT:
+               printf("n ");
+               break;
+          case BLACKBISHOP:
+               printf("b ");
+               break;
+          case BLACKROOK:
+               printf("r ");
+               break;
+          case BLACKQUEEN:
+               printf("q ");
+               break;
+          case BLACKKING:
+               printf("k ");
+               break;
+          }
+     }
      printf("\n");
+}
+
+void main() {
+     board120Setup();
+     printBoard(currentBoard);
 }
