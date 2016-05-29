cab1a3828dded8c079b38b292c3b58a2e7aa6a7e
sjlee235 <sjlee235@gmail.com>
2016-05-29 21:36:22 +0900

    isTerminal tests reverted
    other

--- C++ Chess Engine/C++ Chess Engine/main.cpp
+++ C++ Chess Engine/C++ Chess Engine/main.cpp
@@ -2077,11 +2077,6 @@ void main() {
      
      //  Initialize Board
      board120Setup();
-     //FENboardSetup("k7/8/8/8/8/8/8/7K w - - 70 1");
-     FENboardSetup("k7/8/8/8/8/8/r7/r6K w - - 0 1");
-     printf("%d\n", isTerminalNode(currentBoard));
-     FENboardSetup("k7/8/8/8/8/8/5q2/7K w - - 0 1");
-     printf("%d\n", isTerminalNode(currentBoard));
 
      //  FEN source:
      //  https://chessprogramming.wikispaces.com/Perft+Results
