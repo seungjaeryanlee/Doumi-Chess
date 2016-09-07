267b1019643b29857bba504a2d24d2693475600a
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-07 20:00:47 +0900

    Documented board evaluation functions

--- C++ Chess Engine/C++ Chess Engine/protos.h
+++ C++ Chess Engine/C++ Chess Engine/protos.h
@@ -239,16 +239,51 @@ void printMove(const Move& move);
 
 
 /*                                BOARD EVALUATION                            */
-//  Returns evaluation score based on parameter board given.
+/// <summary>
+/// This function returns evaluation score of the given board using piece values and PCSQ tables. Positive score signifies white's advantage.
+/// </summary>
+/// <param name="board">The board that will be evaluated.</param>
+/// <returns>The score of the board</returns>
 int boardEvaluation(const Board& board);
-//  Gets a position number and returns the row-reversed position number
+/// <summary>
+/// This function returns the row-reversed position of the given position in array index notation. Used in applying PCSQ table for black.
+/// </summary>
+/// <param name="position">The position that will be row-reversed.</param>
+/// <returns>The row-reversed position</returns>
 int reversePosition(int position);
-//  negaMax implemented for board evaluation
+/// <summary>
+/// This function finds the best move using negaMax recursion on the given board for given depth. This function should only be called in rootNegaMax().
+/// </summary>
+/// <param name="depth">The depth of the recursion.</param>
+/// <param name="board">The board that will be analyzed.</param>
+/// <returns>The evaluation score of best move.</returns>
 int negaMax(int depth, Board& board);
-//  function to call negaMax. bestMoves is the output
+/// <summary>
+/// This function is the root function for negaMax().
+/// </summary>
+/// <param name="depth">The depth of the recursion.</param>
+/// <param name="board">The board that will be analyzed.</param>
+/// <param name="bestMove">The best move found.</param>
+/// <returns>The evaluation score of best move.</returns>
 int rootNegaMax(int depth, Board& board, Move& bestMove);
-//  negaMax with alphaBeta pruning implemented for board evaluation
-int alphabeta(int depth, Board& board, int alpha, int beta);
+/// <summary>
+/// This function finds the best move using negaMax recursion and alpha-beta pruning on the given board for given depth. This function should only be called in rootAlphabeta().
+/// </summary>
+/// <param name="maxDepth">The depth of the recursion.</param>
+/// <param name="board">The board that will be analyzed.</param>
+/// <param name="alpha">The alpha value used for alpha-beta pruning.</param>
+/// <param name="beta">The beta value used for alpha-beta pruning.</param>
+/// <returns>The evaluation score of best move.</returns>
+int alphabeta(int maxDepth, Board& board, int alpha, int beta);
+/// <summary>
+/// This function is the root function for negaMax().
+/// </summary>
+/// <param name="maxDepth">The depth of the recursion.</param>
+/// <param name="board">The board that will be analyzed.</param>
+/// <param name="alpha">The alpha value used for alpha-beta pruning.</param>
+/// <param name="beta">The beta value used for alpha-beta pruning.</param>
+/// <param name="bestMove">The best move found.</param>
+/// <returns>The evaluation score of best move.</returns>
 int rootAlphabeta(int maxDepth, Board board, int alpha, int beta, Move& bestMove);
 
 
