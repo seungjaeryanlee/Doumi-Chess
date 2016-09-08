4898c9c6051bfa4b0ab01ccaf4641d7decd7dc33
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-08 09:43:25 -0400

    Change all move[] arrays to movelist class

--- C++ Chess Engine/C++ Chess Engine/movegen.h
+++ C++ Chess Engine/C++ Chess Engine/movegen.h
@@ -1,12 +1,4 @@
 #pragma once
-void moveGeneration(const Board& board, Move legalMoveList[MAX_MOVEGEN_COUNT], int *legalMoveCount);
-void pawnMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void knightMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void bishopMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void rookMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void queenMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void kingMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-
 void moveGeneration(const Board& board, MoveList moveList);
 void pawnMoveGeneration(const Board& board, const int position, MoveList moveList);
 void knightMoveGeneration(const Board& board, const int position, MoveList moveList);
@@ -15,16 +7,9 @@ void rookMoveGeneration(const Board& board, const int position, MoveList moveLis
 void queenMoveGeneration(const Board& board, const int position, MoveList moveList);
 void kingMoveGeneration(const Board& board, const int position, MoveList moveList);
 
-void castlingMoveGeneration(const Board& board, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void promotionMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void enpassantMoveGeneration(const Board& board, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-
 void castlingMoveGeneration(const Board& board, MoveList moveList);
 void promotionMoveGeneration(const Board& board, const int position, MoveList moveList);
 void enpassantMoveGeneration(const Board& board, MoveList moveList);
-
-void addMove(int initial, int terminal, int moveType, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
-void addPromotionMove(int initial, int terminal, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 
 void addMove(int initial, int terminal, int moveType, MoveList moveList);
 void addPromotionMove(int initial, int terminal, MoveList moveList);
