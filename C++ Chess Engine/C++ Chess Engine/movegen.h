e94ab4c27eccbc1d2c8d719931799ec3ea7eb120
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-08 09:26:40 -0400

    Modify movegen.h to make the code compile

--- C++ Chess Engine/C++ Chess Engine/movegen.h
+++ C++ Chess Engine/C++ Chess Engine/movegen.h
@@ -7,11 +7,25 @@ void rookMoveGeneration(const Board& board, const int position, Move moveList[MA
 void queenMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 void kingMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 
+void pawnMoveGeneration(const Board& board, const int position, MoveList moveList);
+void knightMoveGeneration(const Board& board, const int position, MoveList moveList);
+void bishopMoveGeneration(const Board& board, const int position, MoveList moveList);
+void rookMoveGeneration(const Board& board, const int position, MoveList moveList);
+void queenMoveGeneration(const Board& board, const int position, MoveList moveList);
+void kingMoveGeneration(const Board& board, const int position, MoveList moveList);
+
 void castlingMoveGeneration(const Board& board, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 void promotionMoveGeneration(const Board& board, const int position, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 void enpassantMoveGeneration(const Board& board, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 
+void castlingMoveGeneration(const Board& board, MoveList moveList);
+void promotionMoveGeneration(const Board& board, const int position, MoveList moveList);
+void enpassantMoveGeneration(const Board& board, MoveList moveList);
+
 void addMove(int initial, int terminal, int moveType, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
 void addPromotionMove(int initial, int terminal, Move moveList[MAX_MOVEGEN_COUNT], int *moveCount);
+
+void addMove(int initial, int terminal, int moveType, MoveList moveList);
+void addPromotionMove(int initial, int terminal, MoveList moveList);
 
 bool squareAttackCheck(Board board, int position);
