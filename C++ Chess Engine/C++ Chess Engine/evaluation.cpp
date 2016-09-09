2c397798f203f0bc0127f7c1f53777748cbe46a3
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-09 15:07:58 -0400

    Separate evaluation functions to new cpp/h

---
+++ C++ Chess Engine/C++ Chess Engine/evaluation.cpp
@@ -0,0 +1,236 @@
+#include "evaluation.h"
+#include "defs.h"
+#include "movegen.h"
+
+int boardEvaluation(const Board& board) {
+     int score = 0;
+     for (int i = 0; i < 120; i++) {
+          switch (board.getSquare(i)) {
+          case WHITEPAWN:
+               score += PAWNVALUE;
+               score += PAWN_PCSQTable.at(i);
+               break;
+          case WHITEKNIGHT:
+               score += KNIGHTVALUE;
+               score += KNIGHT_PCSQTable.at(i);
+               break;
+          case WHITEBISHOP:
+               score += BISHOPVALUE;
+               score += BISHOP_PCSQTable.at(i);
+               break;
+          case WHITEROOK:
+               score += ROOKVALUE;
+               score += ROOK_PCSQTable.at(i);
+               break;
+          case WHITEQUEEN:
+               score += QUEENVALUE;
+               score += QUEEN_PCSQTable.at(i);
+               break;
+          case WHITEKING:
+               score += KINGVALUE;
+               if (board.getEndgame()) {
+                    score += KING_PCSQTable_ENDGAME.at(i);
+               }
+               else {
+                    score += KING_PCSQTable.at(i);
+               }
+               break;
+          case BLACKPAWN:
+               score -= PAWNVALUE;
+               score -= PAWN_PCSQTable.at(reversePosition(i));
+               break;
+          case BLACKKNIGHT:
+               score -= KNIGHTVALUE;
+               score -= KNIGHT_PCSQTable.at(reversePosition(i));
+               break;
+          case BLACKBISHOP:
+               score -= BISHOPVALUE;
+               score -= BISHOP_PCSQTable.at(reversePosition(i));
+               break;
+          case BLACKROOK:
+               score -= ROOKVALUE;
+               score -= ROOK_PCSQTable.at(reversePosition(i));
+               break;
+          case BLACKQUEEN:
+               score -= QUEENVALUE;
+               score -= QUEEN_PCSQTable.at(reversePosition(i));
+               break;
+          case BLACKKING:
+               score -= KINGVALUE;
+               if (board.getEndgame()) {
+                    score -= KING_PCSQTable_ENDGAME.at(reversePosition(i));
+               }
+               else {
+                    score -= KING_PCSQTable.at(reversePosition(i));
+               }
+               break;
+          }
+     }
+     return score;
+}
+int reversePosition(const int position) {
+     int row = position / 10, column = position % 10;
+     return (12 - row - 1) * 10 + column;
+}
+
+int negaMax(const int depth, Board& board) {
+     gameState state = checkGameState(board);
+     if (state != NOTMATE) {
+          printf("Terminal node!: %d\n", depth);
+          switch (state) {
+          case WHITE_CHECKMATE:
+               return -1 * (MATE_VALUE + depth);
+               break;
+          case BLACK_CHECKMATE:
+               return (MATE_VALUE + depth);
+               break;
+          case STALEMATE_3F:
+          case STALEMATE_50:
+          case STALEMATE_75:
+               return 0;
+               break;
+          }
+     }
+     if (depth == 0) {
+          return board.getTurn() * boardEvaluation(board);
+     }
+     int max_Score = INT_MIN;
+     int score;
+     int capturedPiece;
+
+     MoveList moveList = moveGeneration(board);
+
+     for (int i = 0; i <moveList.getCounter(); i++) {
+
+          updateCastling(board, moveList.getMove(i));
+          int enpassantSquare = board.getEnpassantSquare();
+
+          capturedPiece = makeMove(board, moveList.getMove(i));
+
+          score = -negaMax(depth - 1, board);
+
+          if (score > max_Score) {
+               max_Score = score;
+          }
+
+          undoMove(board, moveList.getMove(i), capturedPiece);
+          board.setEnpassantSquare(enpassantSquare);
+     }
+
+     return max_Score;
+}
+int rootNegaMax(const int maxDepth, Board& board, Move& bestMove) {
+
+     int max_Score = INT_MIN;
+     int score;
+     int capturedPiece;
+
+     MoveList moveList = moveGeneration(board);
+
+     for (int i = 0; i < moveList.getCounter(); i++) {
+          updateCastling(board, moveList.getMove(i));
+
+          int enpassantSquare = board.getEnpassantSquare();
+          capturedPiece = makeMove(board, moveList.getMove(i));
+
+          score = -negaMax(maxDepth - 1, board);
+
+          if (score > max_Score) {
+               max_Score = score;
+               bestMove = moveList.getMove(i);
+          }
+
+          undoMove(board, moveList.getMove(i), capturedPiece);
+          board.setEnpassantSquare(enpassantSquare);
+     }
+
+     return max_Score;
+}
+
+int alphabeta(const int depth, Board& board, int alpha, int beta) {
+     gameState state = checkGameState(board);
+     if (state != NOTMATE) {
+          printf("Terminal node!: %d\n", depth);
+          switch (state) {
+          case WHITE_CHECKMATE:
+               return -1 * (MATE_VALUE + depth);
+               break;
+          case BLACK_CHECKMATE:
+               return (MATE_VALUE + depth);
+               break;
+          case STALEMATE_3F:
+          case STALEMATE_50:
+          case STALEMATE_75:
+               return 0;
+               break;
+          }
+     }
+     if (depth == 0) {
+          return board.getTurn() * boardEvaluation(board);
+     }
+
+     int score;
+     int capturedPiece;
+
+     MoveList moveList = moveGeneration(board);
+
+     for (int i = 0; i < moveList.getCounter(); i++) {
+
+          updateCastling(board, moveList.getMove(i));
+
+          // Save enpassantSquare so it doesn't get lost while making move
+          int enpassantSquare = board.getEnpassantSquare();
+
+          capturedPiece = makeMove(board, moveList.getMove(i));
+
+          score = -alphabeta(depth - 1, board, -beta, -alpha);
+
+          if (score >= beta) {
+               undoMove(board, moveList.getMove(i), capturedPiece);
+               board.setEnpassantSquare(enpassantSquare);
+               return beta;
+          }
+
+          if (score > alpha) {
+               alpha = score;
+          }
+          undoMove(board, moveList.getMove(i), capturedPiece);
+          board.setEnpassantSquare(enpassantSquare);
+     }
+
+     return alpha;
+}
+int rootAlphabeta(const int maxDepth, Board board, int alpha, int beta, Move& bestMove) {
+     int score;
+     int capturedPiece;
+
+     MoveList moveList = moveGeneration(board);
+
+     for (int i = 0; i < moveList.getCounter(); i++) {
+
+          updateCastling(board, moveList.getMove(i));
+          int enpassantSquare = board.getEnpassantSquare();
+          capturedPiece = makeMove(board, moveList.getMove(i));
+
+          score = -alphabeta(maxDepth - 1, board, -beta, -alpha);
+
+          // TODO: Check if this is needed and change it
+          if (score >= beta) {
+
+               undoMove(board, moveList.getMove(i), capturedPiece);
+               // TODO: Make sure Castling & EP Square & other details are also undo-ed
+               board.setEnpassantSquare(enpassantSquare);
+               return beta;
+          }
+
+          if (score > alpha) {
+               alpha = score;
+               bestMove = moveList.getMove(i);
+          }
+
+          undoMove(board, moveList.getMove(i), capturedPiece);
+          board.setEnpassantSquare(enpassantSquare);
+     }
+
+     return alpha;
+}
--- C++ Chess Engine/C++ Chess Engine/protos.h
+++ C++ Chess Engine/C++ Chess Engine/protos.h
@@ -257,53 +257,6 @@ std::string numberToFilerank(const int position);
 
 
 
-/*                                BOARD EVALUATION                            */
-/// <summary>
-/// This function returns evaluation score of the given board using piece values and PCSQ tables. Positive score signifies white's advantage.
-/// </summary>
-/// <param name="board">The board that will be evaluated.</param>
-/// <returns>The score of the board</returns>
-int boardEvaluation(const Board& board);
-/// <summary>
-/// This function returns the row-reversed position of the given position in board index notation. Used in applying PCSQ table for black.
-/// </summary>
-/// <param name="position">The position that will be row-reversed.</param>
-/// <returns>The row-reversed position</returns>
-int reversePosition(const int position);
-/// <summary>
-/// This function finds the best move using negaMax recursion on the given board for given depth. This function should only be called in rootNegaMax().
-/// </summary>
-/// <param name="depth">The depth of the recursion.</param>
-/// <param name="board">The board that will be analyzed.</param>
-/// <returns>The evaluation score of best move.</returns>
-int negaMax(const int depth, Board& board);
-/// <summary>
-/// This function is the root function for negaMax().
-/// </summary>
-/// <param name="depth">The depth of the recursion.</param>
-/// <param name="board">The board that will be analyzed.</param>
-/// <param name="bestMove">The best move found.</param>
-/// <returns>The evaluation score of best move.</returns>
-int rootNegaMax(const int depth, Board& board, Move& bestMove);
-/// <summary>
-/// This function finds the best move using negaMax recursion and alpha-beta pruning on the given board for given depth. This function should only be called in rootAlphabeta().
-/// </summary>
-/// <param name="maxDepth">The depth of the recursion.</param>
-/// <param name="board">The board that will be analyzed.</param>
-/// <param name="alpha">The alpha value used for alpha-beta pruning.</param>
-/// <param name="beta">The beta value used for alpha-beta pruning.</param>
-/// <returns>The evaluation score of best move.</returns>
-int alphabeta(const int maxDepth, Board& board, int alpha, int beta);
-/// <summary>
-/// This function is the root function for negaMax().
-/// </summary>
-/// <param name="maxDepth">The depth of the recursion.</param>
-/// <param name="board">The board that will be analyzed.</param>
-/// <param name="alpha">The alpha value used for alpha-beta pruning.</param>
-/// <param name="beta">The beta value used for alpha-beta pruning.</param>
-/// <param name="bestMove">The best move found.</param>
-/// <returns>The evaluation score of best move.</returns>
-int rootAlphabeta(const int maxDepth, Board board, int alpha, int beta, Move& bestMove);
 
 
 /*                                   RECURSION                                */
