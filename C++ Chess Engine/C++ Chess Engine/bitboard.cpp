6e19229703cf9d4aa3f3bde9c063360e4e482baa
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-12 12:22:45 -0400

    Create basic evaluation function using BB

--- C++ Chess Engine/C++ Chess Engine/bitboard.cpp
+++ C++ Chess Engine/C++ Chess Engine/bitboard.cpp
@@ -1,4 +1,5 @@
 #include "bitboard.h"
+#include "protos.h"
 
 int bitCount(bitboard bb) {
      int count = 0;
@@ -6,4 +7,24 @@ int bitCount(bitboard bb) {
           bb &= bb - 1;
      }
      return count;
+}
+
+int bitboardEvaluation(Board& board) {
+     // TODO: Improve with Having variables for pieceCounts
+     int score = 0;
+     score = bitCount(board.getWKBB) * WHITEKING
+           + bitCount(board.getWQBB) * WHITEQUEEN
+           + bitCount(board.getWRBB) * WHITEROOK
+           + bitCount(board.getWBBB) * WHITEBISHOP
+           + bitCount(board.getWNBB) * WHITEKNIGHT
+           + bitCount(board.getWPBB) * WHITEPAWN
+           - bitCount(board.getBKBB) * BLACKKING
+           - bitCount(board.getBQBB) * BLACKQUEEN
+           - bitCount(board.getBRBB) * BLACKROOK
+           - bitCount(board.getBNBB) * BLACKBISHOP
+           - bitCount(board.getBNBB) * BLACKKNIGHT
+           - bitCount(board.getBPBB) * BLACKPAWN;
+
+     // TODO: Read and Add PCSQ Table Score
+     
 }
