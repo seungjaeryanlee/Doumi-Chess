8ba28258543b2536c5828a707f1ddcc361c0e7b0
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-13 07:25:31 -0400

    Complete updateScore
    needs testing

--- C++ Chess Engine/C++ Chess Engine/protos.h
+++ C++ Chess Engine/C++ Chess Engine/protos.h
@@ -318,13 +318,60 @@ public:
           return score;
      }
 
-     void updateScore(Move& move) {
-          if (move.getTerminal() != EMPTYSQUARE) {
-               score -= PIECEVALUE[move.getTerminal()];
-               // TODO: Subtract PCSQ Value of Captured Piece
+     void updateScore(Move& move, int capturedPiece) {
+          if (capturedPiece != EMPTYSQUARE) {
+               score -= PIECEVALUE[capturedPiece];
+               switch (capturedPiece) {
+               case WHITEPAWN:
+                    score -= PAWN_PCSQTable.at(move.getTerminal());
+                    break;
+               case WHITEKNIGHT:
+                    score -= KNIGHT_PCSQTable.at(move.getTerminal());
+                    break;
+               case WHITEBISHOP:
+                    score -= BISHOP_PCSQTable.at(move.getTerminal());
+                    break;
+               case WHITEROOK:
+                    score -= ROOK_PCSQTable.at(move.getTerminal());
+                    break;
+               case WHITEQUEEN:
+                    score -= QUEEN_PCSQTable.at(move.getTerminal());
+                    break;
+               case WHITEKING:
+                    if (isEndgame) {
+                         score -= KING_PCSQTable_ENDGAME.at(move.getTerminal());
+                    }
+                    else {
+                         score -= KING_PCSQTable.at(move.getTerminal());
+                    }
+                    break;
+               case BLACKPAWN:
+                    score += PAWN_PCSQTable.at(reversePosition(move.getTerminal()));
+                    break;
+               case BLACKKNIGHT:
+                    score += KNIGHT_PCSQTable.at(reversePosition(move.getTerminal()));
+                    break;
+               case BLACKBISHOP:
+                    score += BISHOP_PCSQTable.at(reversePosition(move.getTerminal()));
+                    break;
+               case BLACKROOK:
+                    score += ROOK_PCSQTable.at(reversePosition(move.getTerminal()));
+                    break;
+               case BLACKQUEEN:
+                    score += QUEEN_PCSQTable.at(reversePosition(move.getTerminal()));
+                    break;
+               case BLACKKING:
+                    if (isEndgame) {
+                         score += KING_PCSQTable_ENDGAME.at(reversePosition(move.getTerminal()));
+                    }
+                    else {
+                         score += KING_PCSQTable.at(reversePosition(move.getTerminal()));
+                    }
+                    break;
+               }
           }
-          // TODO: Add PCSQ Value of Moved Piece at Terminal Pos
-          // TODO: Subtract PCSQ Value of Moved Piece at Terminal Pos
+          score += getPCSQValue(move.getTerminal());
+          score -= getPCSQValue(move.getInitial());
      }
 };
 
