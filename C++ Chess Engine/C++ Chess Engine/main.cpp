5edfb72b292606b621d2b174856ba7da26eb06f1
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-23 23:48:16 -0400

    Uncommitted changes from last commit

--- C++ Chess Engine/C++ Chess Engine/main.cpp
+++ C++ Chess Engine/C++ Chess Engine/main.cpp
@@ -1132,17 +1132,65 @@ void main() {
                     }
                }
                else if (commandType == DEBUG) {
+                    // COMMIT 4445572
+                    // MoveGen 6300 calls: 769 ms, BoardEval 4500 calls: 23 ms
+                    FENboardSetup(currentBoard, "8/8/2N5/4Q3/3R4/5B2/7K/8 w - - 0 1");
 
+                    MoveList moveList;
                     LARGE_INTEGER frequency2, beginTime2, endTime2;
+
                     frequency2 = startTimer(&beginTime2, 2);
-
                     for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         pawnMoveGeneration(currentBoard, E2, moveList);
                     }
-
                     stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 pawnMoveGen.\n";
 
-
+                    
+                    frequency2 = startTimer(&beginTime2, 2);
+                    for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         knightMoveGeneration(currentBoard, B1, moveList);
                     }
+                    stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 knightMoveGen.\n";
+
+
+                    frequency2 = startTimer(&beginTime2, 2);
+                    for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         bishopMoveGeneration(currentBoard, C1, moveList);
+                    }
+                    stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 bishopMoveGen.\n";
+
+
+                    frequency2 = startTimer(&beginTime2, 2);
+                    for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         rookMoveGeneration(currentBoard, A1, moveList);
+                    }
+                    stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 rookMoveGen.\n";
+
+
+                    frequency2 = startTimer(&beginTime2, 2);
+                    for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         queenMoveGeneration(currentBoard, D1, moveList);
+                    }
+                    stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 queenMoveGen.\n";
+
+
+                    frequency2 = startTimer(&beginTime2, 2);
+                    for (int i = 0; i < 6300; i++) {
+                         moveList.setCounterToZero();
+                         kingMoveGeneration(currentBoard, E1, moveList);
+                    }
+                    stopTimer(&endTime2, 2);
+                    std::cout << elapsedTime(beginTime2, endTime2, frequency2, 2) << " ms for 6300 kingMoveGen.\n";
 
                }
           }
