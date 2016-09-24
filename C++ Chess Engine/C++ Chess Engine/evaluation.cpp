4445572d5879718e60021716915de20a5fc2432f
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-09-23 23:31:32 -0400

    Miinor change to delete unneeded output used for debugging

--- C++ Chess Engine/C++ Chess Engine/evaluation.cpp
+++ C++ Chess Engine/C++ Chess Engine/evaluation.cpp
@@ -1,26 +1,26 @@
 #include "evaluation.h"
-#include "defs.h"
 #include "movegen.h"
 
-int alphabeta(const int depth, Board& board, int alpha, int beta) {
-     gameState state = checkGameState(board);
-     if (state != NOTMATE) {
-          printf("Terminal node!: %d\n", depth);
-          switch (state) {
-          case WHITE_CHECKMATE:
-               return -1 * (MATE_VALUE + depth);
-               break;
-          case BLACK_CHECKMATE:
-               return (MATE_VALUE + depth);
-               break;
-          case STALEMATE_3F:
-          case STALEMATE_50:
-          case STALEMATE_75:
-               return 0;
-               break;
-          }
+
+int alphabeta(const int depth, Board& board, int alpha, int beta) {   
+     switch (checkGameState(board)) {
+     case NOTMATE:
+          break;
+     case WHITE_CHECKMATE:
+          return -1 * (MATE_VALUE + depth);
+          break;
+     case BLACK_CHECKMATE:
+          return (MATE_VALUE + depth);
+          break;
+     case STALEMATE_3F:
+     case STALEMATE_50:
+     case STALEMATE_75:
+          return 0;
+          break;
      }
+
      if (depth == 0) {
+          
           return board.getTurn() * board.boardEvaluation();
      }
 
@@ -62,7 +62,7 @@ int rootAlphabeta(const int maxDepth, Board board, int alpha, int beta, Move& be
           updateBoard(board, moveList.getMove(i), capturedPiece);
 
           score = -alphabeta(maxDepth - 1, board, -beta, -alpha);
-
+          
           // TODO: Check if this is needed and change it
           if (score >= beta) {
                board = oldBoard;
