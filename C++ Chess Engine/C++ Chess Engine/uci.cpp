9cda3c0b65fa0d33c7e5d7aeda297b791b6503e5
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-11-03 06:45:41 -0400

    Implement "position" command for UCI

--- C++ Chess Engine/C++ Chess Engine/uci.cpp
+++ C++ Chess Engine/C++ Chess Engine/uci.cpp
@@ -56,16 +56,71 @@ void main() {
           else if (token == "ucinewgame") {
                board120Setup(board);
           }
+          else if (token == "position") {
+               std::string option;
+               is >> std::skipws >> option;
+               
+               // Find Starting position
+               if (option == "startpos") {
+                    board120Setup(board);
+               }
+               else if (option == "fen") { // FEN is given
+                    FENboardSetup(board, option);
+               }
+               else { return; }
 
+               // Check if there are any moves given
+               is >> std::skipws >> option;
+               if (option == "moves") {
+                    // Do given moves to the board
+                    std::string moveString;
+                    Move move;
+                    while (is >> moveString) {
+                         // Save Board
+                         savedBoard[saveIndex] = board;
+                         saveIndex++;
+
+                         // Interpret String
+                         move.setInitial(filerankToNumber(moveString[0], moveString[1] - '0'));
+                         move.setTerminal(filerankToNumber(moveString[2], moveString[3] - '0'));
+
+                         if (moveString.length() == 4) {
+                              // TODO: Give correct moveType?
+                              move.setType(NORMAL);
+                         }
+                         else if (moveString[4] == 'n') {
+                              move.setType(KNIGHT_PROMOTION);
+                         }
+                         else if (moveString[4] == 'b') {
+                              move.setType(BISHOP_PROMOTION);
+                         }
+                         else if (moveString[4] == 'r') {
+                              move.setType(ROOK_PROMOTION);
+                         }
+                         else if (moveString[4] == 'q') {
+                              move.setType(QUEEN_PROMOTION);
+                         }
+                         else { return; }
+
+                         // Make move on the board
+                         makeMove(board, move);
+                    }
+               }
+
+               
+          }
 
           // 3. UNIMPLEMENTED TOKENS
           else if (token == "debug") {}
           else if (token == "setoption") {}
           else if (token == "register") {}
-          
-          else if (token == "position") {}
           else if (token == "stop") {}
           else if (token == "ponderhit") {}
+
+          // 4. DEBUGGING TOKENS (NOT FOR UCI)
+          else if (token == "printfen") {
+               boardToFEN(board);
+          }
 
      }
 }
